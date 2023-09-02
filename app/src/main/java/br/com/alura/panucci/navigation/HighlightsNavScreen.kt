038fbfe0c8567c8d1ab50de907a5325ec869aa76
Cainã Oliveira <csoliveira01@gmail.com>
2023-09-02 13:58:52 -0400

    Integrating views and view models

--- app/src/main/java/br/com/alura/panucci/navigation/HighlightsNavScreen.kt
+++ app/src/main/java/br/com/alura/panucci/navigation/HighlightsNavScreen.kt
@@ -1,18 +1,23 @@
 package br.com.alura.panucci.navigation
 
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import br.com.alura.panucci.sampledata.sampleProducts
 import br.com.alura.panucci.ui.screens.HighlightsListScreen
+import br.com.alura.panucci.ui.viewmodels.HighlightsListViewModel
 
 internal const val highlightsRoute = "highlights"
 fun NavGraphBuilder.highlightsNavScreen(navController: NavHostController) {
     composable(highlightsRoute) {
+        val viewModel = viewModel<HighlightsListViewModel>()
+        val uiState by viewModel.uiState.collectAsState()
         HighlightsListScreen(
-            products = sampleProducts,
+            uiState = uiState,
             onNavigateToDetails = { product ->
                 val discount = "ALURA"
                 navController.navigateToProductDetails(product.id, discount)
