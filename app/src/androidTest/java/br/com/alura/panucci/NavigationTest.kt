dfa97a162b3227d9b5a3fb4872b0cd8c95b7c57d
Cainã Oliveira <csoliveira01@gmail.com>
2023-09-07 19:26:14 -0400

    Starting implementing tests for navigation

---
+++ app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
@@ -0,0 +1,55 @@
+package br.com.alura.panucci
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import br.com.alura.panucci.navigation.PanucciNavHost
+import br.com.alura.panucci.navigation.menuRoute
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupAppNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            PanucciNavHost(navController = navController)
+        }
+    }
+
+    // Unit test
+    @Test
+    fun appNavHost_verifyStartDestination() {
+        composeTestRule.onRoot().printToLog("PannucciApp")
+        composeTestRule
+            .onNodeWithText("Destaques do dia")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun appNavHost_verifyIfMenuScreenIsDisplayed() {
+        composeTestRule.onRoot().printToLog("PannucciApp")
+        composeTestRule.onNodeWithText("Menu").performClick()
+
+        composeTestRule.onNodeWithText("Menu")
+            .assertIsDisplayed()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        Assert.assertEquals(route, menuRoute)
+    }
+
+}
